3d7cb85395f31b5a3e029d4ad55a9fdb666af574
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 20:46:04 +0200

    added manage meeting componenet with  text heading

--- src/components/ManageMeeting.jsx
+++ src/components/ManageMeeting.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Form from './Form';
+import MeetingList from './MeetingList';
+import { useState,useEffect } from 'react';
+
 
 
 const ManageMeeting = () => {
+   
     return (
        
-           <Form/>
+      <div  className='container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4' > 
+      <h3 className='text-center mt-3'>Manage Meeting</h3>
+  </div>  
        
     );
 };
