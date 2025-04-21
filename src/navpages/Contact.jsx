dfb3a729c6854416646653e71c675ffe573ee8dc
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:02:42 +0200

    added one-line description and margin style

--- src/navpages/Contact.jsx
+++ src/navpages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="main-section">
-           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora nesciunt porro similique nemo debitis illum quibusdam excepturi itaque! Dolores facilis inventore sunt laboriosam odio corrupti reprehenderit deserunt animi similique debitis!</p> 
+           <p className='mt-3 mx-3'>Have questions or need support? Reach out anytime. </p> 
         </div>
     );
 };
