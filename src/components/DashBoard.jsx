4d39c07346c58d38207859ca15b62163e09fa233
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 11:46:32 +0200

    added Form creation and meeting list display

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -6,90 +6,77 @@ import Notification from './Notification';
 import Settings from './Settings';
 import ScheduleMeeting from './ScheduleMeeting';
 import ManageMeeting from './ManageMeeting';
-import { Routes,Route } from 'react-router';
-import { useState,useEffect } from 'react';
-import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { useState ,useEffect} from 'react';
 import { useNavigate } from 'react-router';
+import axios from 'axios';
 
 
 
 const DashBoard = () => {
 
-const [meetings,setMeetings]=useState([]);
-const [activeLink, setActiveLink] = useState(""); 
-const [edit,setEdit]=useState(false);
-const [id,setId]=useState();
-  const [modalIsOpen,setModalIsOpen]=useState(false);
+  const [meetings, setMeetings] = useState([]);
+  const [activeLink, setActiveLink] = useState("");
+  const navigate = useNavigate();
 
-const navigate=useNavigate();
-const API_ENDPOINT="http://localhost:8080/api/meetingCalendar"
-console.log("DashBoard component rendered");
-useEffect(()=>{
-    console.log(" Dfecth all meetings");
-    fetchAllMeetings();
-
-},[]);
-
-
-
-const fetchAllMeetings=async()=>{
-  await axios.get(API_ENDPOINT)
-  .then((response)=>{
-    console.log("Get Response",response);
-    if(response.status === 200){
-        setMeetings(response.data);
-    }else{
-        console.error("Error response status for Get API",response.status); 
-    }
-    
-  }).catch((error)=>{
-    console.error("Error occurred while fetch all meetings",error);
-  })
+  const API_ENDPOINT="http://localhost:8080/api/meetingCalendar"
+  console.log("DashBoard component rendered");
   
-};
+
+  useEffect(()=>{
+      console.log(" Dfecth all meetings");
+      fetchAllMeetings();
+  
+  },[]);
 
 
-const updatedMeeting = (updatedMeetings) => {
+  const fetchAllMeetings = async () => {
+    await axios.get(API_ENDPOINT)
+      .then((response) => {
+        console.log("Get Response", response);
+        if (response.status === 200) {
+          setMeetings(response.data);
+        } else {
+          console.error("Error response status for Get API", response.status);
+        }
+
+      }).catch((error) => {
+        console.error("Error occurred while fetch all meetings", error);
+      })
+
+  };
+
+
+  const updatedMeeting = (updatedMeetings) => {
     setMeetings(updatedMeetings);
   };
 
-const onEdit=(meetingId)=>{
-    setEdit(true);
-    setId(meetingId);
-    navigate(`/dashboard/manage-meeting/${meetingId}`);
- }
 
-
- const onDelete= (meetingId)=>{
-    setId(meetingId);
-    setModalIsOpen(true);
- }
-
- const handleNavigate = (route) => {
+  const handleNavigate = (route) => {
     setActiveLink(route);
-    navigate(route); 
+    navigate(route);
   };
-    
-
-    return (
-        <div className='main-section'  >
-            <Sidebar activeLink={activeLink} onNavigate={handleNavigate}/>
 
 
-        <Routes>
-          <Route path="schedule-meeting" element={<ScheduleMeeting meetings={meetings}  updatedMeeting={updatedMeeting}  onEdit={onEdit}  onDelete={onDelete} edit={edit}/>} />
-         <Route path="manage-meeting/:meetingId" element={<ScheduleMeeting  meetings={meetings}  updatedMeeting={updatedMeeting}  onEdit={onEdit}  onDelete={onDelete}  edit={edit}/>} />
-         <Route path="manage-meeting" element={<ManageMeeting meetings={meetings} onEdit={onEdit}  onDelete={onDelete}/>} />
-          <Route path="users-permissions" element={<UserPermission />} />
-          <Route path="notifications" element={<Notification />} />
-          <Route path="analytics" element={<Analytics />} />
-          <Route path="settings" element={<Settings />} />
-         </Routes>
-        
-           
-            
-        </div>
-    );
+  return (
+    <div className='main-section'  >
+      <Sidebar activeLink={activeLink} onNavigate={handleNavigate} />
+
+
+      <Routes>
+        <Route path="schedule-meeting" element={<ScheduleMeeting  meetings={meetings} updatedMeeting={updatedMeeting} />} />
+        <Route path="manage-meeting/:meetingId" element={<ScheduleMeeting />} />
+        <Route path="manage-meeting" element={<ManageMeeting />} />
+        <Route path="users-permissions" element={<UserPermission />} />
+        <Route path="notifications" element={<Notification />} />
+        <Route path="analytics" element={<Analytics />} />
+        <Route path="settings" element={<Settings />} />
+      </Routes>
+
+
+
+    </div>
+  );
 };
 
 export default DashBoard;
--- src/components/MeetingList.jsx
+++ src/components/MeetingList.jsx
@@ -1,89 +1,43 @@
-
 import React from 'react';
-import { useNavigate } from 'react-router';
 
+const MeetingList = ({ meetings }) => {
 
-const MeetingList = ({meetings,onEdit,onDelete}) => {
+    return (
+        <div className="container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4">
 
+            <h3 className='py-4'>List of Created Meetings</h3>
+            <div className="table-responsive">
+                <table className="table table-striped p-4">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Meeting Title</th>
+                            <th>Meeting Date</th>
+                            <th>Meeting Time</th>
+                            <th>Level</th>
+                            <th>Actions</th>
 
-    /*const [meetings,setMeetings]=useState([]);
-    
-    const API_ENDPOINT="http://localhost:8080/api/meetingCalendar"
-
-    useEffect(()=>{
-   fetchAllMeetings();
-    },[])
-
-    const fetchAllMeetings=async()=>{
-              await axios.get(API_ENDPOINT)
-              .then((response)=>{
-                console.log(response);
-                if(response.status===200){
-                    setMeetings(response.data); 
-                }else{
-                    console.error("Error response status",response.status);
-                }             
-                })
-              .catch((error)=>{
-                console.error("error occurred");
-              });
-    }
-
-    const updateMeeting1=async(updatedMeeting)=>{
-        try{
-        const response=await axios.put(API_ENDPOINT,updatedMeeting);
-        if(response.status===204){
-            fetchAllMeetings();
-            console.log("Meeting updated");
-        }
-    }catch(error){
-       console.error("Error occurred while updating");
-    }
-    }*/
-
-   
-  
-
-
-   
-    return( 
-    
-    <div className="container bg-white mt-5">
-      
-    <h3>List of Scheduled Meetings</h3>
-    <div className="table-responsive"> {/* Added table-responsive class */}
-        <table className="table table-striped p-4">
-            <thead>
-                <tr>
-                <th>#</th>
-                    <th>Meeting Title</th>
-                    <th>Meeting Date</th>
-                    <th>Meeting Time</th>
-                    <th>Level</th>
-                    <th>Actions</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                {meetings.map((meeting, index) => (
-                    <tr key={index}>
-                         <td>{index + 1}</td>
-                        <td>{meeting.meetingTitle}</td>
-                        <td>{meeting.meetingDate}</td>
-                        <td>{meeting.meetingTime}</td>
-                        <td>{meeting.meetingLevel}</td>
-                         <td>
-                         <button className="me-1 btn btn-warning" onClick={()=>onEdit(meeting.meetingId)}><i class="bi bi-pencil-square"></i></button>
-                        <button className="btn btn-danger" onClick={()=>onDelete(meeting.meetingId)}><i class="bi bi-trash"></i></button>
-                            </td>                       
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-    </div>
-
-);
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {meetings.map((meeting, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{meeting.meetingTitle}</td>
+                                <td>{meeting.meetingDate}</td>
+                                <td>{meeting.meetingTime}</td>
+                                <td>{meeting.meetingLevel}</td>
+                                <td>
+                                    <button className="me-1 btn btn-warning" ><i className="bi bi-pencil-square"></i></button>
+                                    <button className="btn btn-danger" ><i className="bi bi-trash"></i></button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default MeetingList;
