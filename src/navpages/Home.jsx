9a1e6e91aa225c83a8a5fc7001428b60a1734980
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:03:08 +0200

    added one-line description

--- src/navpages/Home.jsx
+++ src/navpages/Home.jsx
@@ -2,29 +2,25 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 const Home = () => {
-    const navigate = useNavigate();
-    return (
-        <div className='container' style={{  height: "100vh" }}>
-          <h1>Welcome to the Meeting Calendar</h1>
-      
-      <p>
-        Apersonal meeting calendar, where you can easily schedule, manage, and keep track of all your upcoming meetings.</p>
-       <p>  
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor facere quas perferendis id doloribus voluptas tenetur quos quaerat consequuntur rem, velit ex nihil blanditiis possimus officia itaque repellendus, aperiam dicta?
-      </p>
+  const navigate = useNavigate();
+  return (
+    <div className='container' style={{ height: "100vh" }}>
+      <h2 className='mt-3'>Welcome to the Meeting Calendar</h2>
 
-      
+      <p>
+        A personal meeting calendar, where you can easily schedule, manage, and keep track of all your upcoming meetings.</p>
+
       <p>
         Click below to access your dashboard and start scheduling your first meeting!
-      </p>  
+      </p>
       <p><button
-          className="btn btn-outline-primary"
-          onClick={() => navigate("/dashboard")}
-        >
-          Dashboard
-        </button></p>
-        </div>
-    );
+        className="btn btn-outline-primary"
+        onClick={() => navigate("/dashboard")}
+      >
+        Dashboard
+      </button></p>
+    </div>
+  );
 };
 
 export default Home;
