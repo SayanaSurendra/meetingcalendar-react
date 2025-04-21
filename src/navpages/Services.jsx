c280dba8b5dec59d1466f92f96a67302eb93e438
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:03:21 +0200

    added one-line description and margin style

--- src/navpages/Services.jsx
+++ src/navpages/Services.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Services = () => {
     return (
         <div className="main-section">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit necessitatibus reiciendis nisi, iusto eos quo odio omnis possimus obcaecati repellendus id vitae repudiandae cupiditate ipsa. Molestiae necessitatibus sed nostrum totam.</p>  
+          <p className='mt-3 mx-3'>Easily schedule meetings,update details, and stay notified.</p> 
         </div>
     );
 };
