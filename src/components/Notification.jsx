b9cdf05cd98832ab7a19cf642780b80eeda6acfc
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:51:31 +0200

    added notifications component -container and text styles

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Notification = () => {
     return (
-        <div>
-            
-        </div>
+        <div  className='container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4' > 
+         <h3 className='text-center mt-3'>Notification</h3>
+    </div>
     );
 };
 
