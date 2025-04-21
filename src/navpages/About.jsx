aafc76122eac9e8b0254cafdae481f8729797154
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:02:11 +0200

    added one-line description

--- src/navpages/About.jsx
+++ src/navpages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className="main-section">
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum quod id reprehenderit, commodi suscipit quidem. Odio voluptas consequatur dolor, excepturi rem at iste natus ducimus ex inventore perferendis atque modi?</p>  
+          <p className='mt-3 mx-3'> Meeting Calendar is designed to simplify team collaboration and meeting management. Plan effortlessly, organize, and track meetings in one central place.</p>  
         </div>
     );
 };
