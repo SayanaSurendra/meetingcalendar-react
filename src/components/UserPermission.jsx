586728f4b2be4242dea2f9522d2df8806db0852e
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:50:47 +0200

    added user and permission component -container and text styles

--- src/components/UserPermission.jsx
+++ src/components/UserPermission.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const UserPermission = () => {
     return (
-        <div>
-            
+        <div  className='container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4' > 
+            <h3 className='text-center mt-3'>Users & Permissions</h3>
         </div>
     );
 };
