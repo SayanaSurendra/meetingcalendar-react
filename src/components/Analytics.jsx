66e09b02660264b882bc0e15d5f75d49bc881cfe
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:50:24 +0200

    added analytics component -container and text styles

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Analytics = () => {
     return (
-        <div>
-            <h2>Analytics</h2>
+        
+        <div  className='container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4'>
+            <h3 className='text-center mt-3'>Analytics</h3>
         </div>
     );
 };
