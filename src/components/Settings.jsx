1f5851f00c837019db28019ba271603d78b77b18
Sayana Surendran <sayanasurendran31@gmail.com>
2025-04-21 10:51:04 +0200

    added settings component -container and text styles

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Settings = () => {
     return (
-        <div>
-            
-        </div>
+        <div  className='container-fluid bg-white ms-3 me-5 px-4 mt-4 mb-4'>
+         <h3 className='text-center mt-3'>Settings</h3>
+    </div>
     );
 };
 
